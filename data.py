fe42be2a275f9c816a8984dbde6e9558ae91955c
Ashish Amar <ashishamar1999@gmail.com>
2020-11-28 08:06:15 +0530

    Update data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+username = 'foo-bar' #Your Udemy username goes here.
+
+login_link = 'https://www.udemy.com/join/login-popup/?locale=en_US&response_type=html&next=https%3A%2F%2Fwww.udemy.com%2F'
+
+password = 'eggs-spam' #Your Udemy password goes here.
+
+courses_link_sorted = 'https://www.udemy.com/home/my-courses/learning/?sort=title&p='
